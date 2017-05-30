56ff6d309a8afd3a695114d0c31bc2a95670d0ba
henaege <henaege@gmail.com>
2017-05-30 11:07:03 -0400

    add index.html and error handling if it doesn't exist

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ var fs = require("fs");
 
 var server = http.createServer((req, res)=> {
 console.log("Someone connected via http.")
+fs.readFile('index.html', 'utf-8', (error, data)=> {
+    console.log(error);
+    console.log(data);
+    if (error) {
+        res.writeHead(500, {'content-type':'text/html'});
+        res.end('Internal server error');
+    } else {
+        res.writeHead(200,{'content-type':'text/html'});
+        res.end(data);
+    }
+})
 });
 
 server.listen(8080);
